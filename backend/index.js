2b6065ab24a1a4b9da1526147b3baa99daf34bf1
Will-Hsu <46422881+Will-Hsu@users.noreply.github.com>
2023-06-24 09:19:22 -0700

    Initialize Web Dev Project (React-FE, Node.js-BE)

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// initialize the application
+const app = express();
+
+// load environment variables
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", function (_, res) {
+  res.send("Hello World");
+});
+
+var server = app.listen(PORT, "127.0.0.1", function () {
+  var port = server.address().port;
+
+  console.log("Example app listening at http://localhost:%s", port);
+});
