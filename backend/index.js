1c06491009cf1da8ab49f450e4cb7eeb2737298a
Will-Hsu <46422881+Will-Hsu@users.noreply.github.com>
2023-06-24 10:28:16 -0700

    Add eslint and prettier configurations

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 // initialize the application
 const app = express();
@@ -10,18 +10,17 @@ const app = express();
 // load environment variables
 dotenv.config();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", function (_, res) {
-  res.send("Hello World");
+app.get('/', function (_, res) {
+  res.send('Hello World');
 });
 
-var server = app.listen(PORT, "127.0.0.1", function () {
+var server = app.listen(PORT, '127.0.0.1', function () {
   var port = server.address().port;
-
-  console.log("Example app listening at http://localhost:%s", port);
+  console.log('Example app listening at http://localhost:%s', port);
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>CSES website</header>
+    <div className="App">
+      <header className="App-header">CSES website</header>
     </div>
   );
 }
