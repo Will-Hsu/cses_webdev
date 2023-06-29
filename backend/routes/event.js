a75e8b2ff8448576669907f887ac3c873dc67e60
Eddie Ho <noodleslove@icloud.com>
2023-06-28 22:19:10 -0700

    Create Event routes

---
+++ backend/routes/event.js
@@ -0,0 +1,25 @@
+import express from 'express';
+const router = express.Router();
+
+// Require controller modules.
+import eventController from '../controllers/eventController.js';
+
+/// EVENT ROUTES ///
+
+// GET request for list of all Event items.
+router.get('/events', eventController.eventList);
+
+// GET request for one Event.
+router.get('/event/:id', eventController.eventDetail);
+
+// POST request for creating Event.
+router.post('/event/create', eventController.eventCreate);
+
+// POST request to delete Event.
+router.delete('/event/:id/delete', eventController.eventDelete);
+
+// POST request to update Event.
+router.put('/event/:id/update', eventController.eventUpdate);
+
+// Export router.
+export default router;
