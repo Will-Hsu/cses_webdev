b6e3d0eacfbab51a0a76148dfff1d83cd8d72d53
SadracSantacruz <sadrac.santacruz@gmail.com>
2023-06-29 15:56:32 -0700

    fixed some bugs

---
+++ frontend/src/components/Events/styles.ts
@@ -0,0 +1,10 @@
+export const event_style = () => ({
+  button: { color: 'white', textTransform: 'none', fontFamily: 'Inter, sans-serif' },
+  listitem: {
+    '&:hover': {
+      background: 'rgba(255, 255, 255, 0.1)',
+    },
+  },
+  menuicon: { fontSize: '35px', margin: '10px' },
+  closeicon: { color: 'white', fontSize: '35px', margin: '10px 26px' },
+});
