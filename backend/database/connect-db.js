66472d1b3a6adfd5f6cf4328d9e13aaa98f314ce
Brian Liu <bwliu@outlook.com>
2023-06-29 23:31:15 -0700

    Setup MongoDB database connection

---
+++ backend/database/connect-db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+// MongoDB server url
+const uri = process.env.CONNECTION_URL;
+
+// Connect to database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './database/connect-db.js';
+// import testDatabaseConnection from './database/db-test.js';
 
 // import routes
 import eventRoutes from './routes/event.js';
@@ -19,6 +21,12 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 const baseApi = '/api/v1';
+
+// Connect to MongoDB
+connectDB();
+
+// For testing database connection
+// testDatabaseConnection();
 
 app.get('/', function (_, res) {
   res.send('CSES Web Dev Backend');
