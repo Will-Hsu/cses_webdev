72c7e9cda01da264fc57db3b7727c32e0767cbc0
Will-Hsu <46422881+Will-Hsu@users.noreply.github.com>
2023-07-01 16:43:50 -0700

    add alerts for invalid input

--- frontend/src/components/Footer/Forms.tsx
+++ frontend/src/components/Footer/Forms.tsx
@@ -1,24 +1,70 @@
 import React, { useState } from 'react';
-import { Button, Typography, TextField, useTheme } from '@mui/material';
+import { Alert, Button, Typography, TextField, useTheme } from '@mui/material';
 import { footerStyles } from './styles';
 
 const Forms = () => {
   const theme = useTheme();
   const styles = footerStyles(theme);
   const [newsletterEmail, setNewsletterEmail] = useState('');
-  const [validEmail, setValidEmail] = useState(true);
+  const [showError, setShowError] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showErrorForm, setShowErrorForm] = useState(false);
+  const [showSuccessForm, setShowSuccessForm] = useState(false);
   const [feedbackForm, setFeedbackForm] = useState({
     name: '',
     email: '',
     title: '',
     message: '',
   });
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  const subscribeNewLetter = () => {};
+  const subscribeNewLetter = () => {
+    setShowError(false);
+    setShowSuccess(false);
+    if (newsletterEmail === '') return;
+    if (emailRegex.test(newsletterEmail)) {
+      setNewsletterEmail('');
+      setShowSuccess(true);
+      setTimeout(function () {
+        setShowSuccess(false);
+      }, 5000);
+    } else {
+      setShowError(true);
+    }
+  };
+
+  const sendForm = () => {
+    setShowErrorForm(false);
+    setShowSuccessForm(false);
+    if (feedbackForm.name === '' || feedbackForm.email === '' || feedbackForm.message === '')
+      return;
+    if (emailRegex.test(feedbackForm.email)) {
+      setFeedbackForm({
+        name: '',
+        email: '',
+        title: '',
+        message: '',
+      });
+      setShowSuccessForm(true);
+      setTimeout(function () {
+        setShowSuccessForm(false);
+      }, 5000);
+    } else {
+      setShowErrorForm(true);
+    }
+  };
 
   return (
     <div style={{ maxWidth: '530px' }}>
       <Typography sx={styles.title}>Subscribe to our newsletter!</Typography>
+      {showSuccess && (
+        <Alert severity="success">Thank you for subscribing to our newsletter!</Alert>
+      )}
+      {showError && (
+        <Alert severity="error">
+          Invalid email — <strong>please re-enter a valid address!</strong>
+        </Alert>
+      )}
       <TextField
         sx={{
           ...styles.textfield,
@@ -28,8 +74,8 @@ const Forms = () => {
           },
         }}
         size="small"
-        error={!validEmail}
-        placeholder={'Enter Email Address'}
+        placeholder={'Enter Email Address*'}
+        value={newsletterEmail}
         onChange={(e) => setNewsletterEmail(e.target.value)}
       />
       <Button
@@ -44,6 +90,12 @@ const Forms = () => {
         Subscribe
       </Button>
       <Typography sx={styles.title}>Contact us</Typography>
+      {showSuccessForm && <Alert severity="success">Thank you for your feedback!</Alert>}
+      {showErrorForm && (
+        <Alert severity="error">
+          Invalid email — <strong>please re-enter a valid address!</strong>
+        </Alert>
+      )}
       <TextField
         size="small"
         sx={{
@@ -53,7 +105,8 @@ const Forms = () => {
             width: '95%',
           },
         }}
-        placeholder={'Name'}
+        value={feedbackForm.name}
+        placeholder={'Name*'}
         onChange={(e) => setFeedbackForm({ ...feedbackForm, name: e.target.value })}
       />
       <TextField
@@ -65,25 +118,28 @@ const Forms = () => {
             width: '95%',
           },
         }}
-        placeholder={'Email'}
+        placeholder={'Email*'}
+        value={feedbackForm.email}
         onChange={(e) => setFeedbackForm({ ...feedbackForm, email: e.target.value })}
       />
       <TextField
         sx={styles.textfield}
         size="small"
         placeholder={'Title'}
+        value={feedbackForm.title}
         style={{ width: '95%' }}
         onChange={(e) => setFeedbackForm({ ...feedbackForm, title: e.target.value })}
       />
       <TextField
         sx={styles.textfield}
-        placeholder={'Tell us what you think!'}
+        placeholder={'Tell us what you think!*'}
         style={{ width: '95%' }}
         rows={3}
         multiline
+        value={feedbackForm.message}
         onChange={(e) => setFeedbackForm({ ...feedbackForm, message: e.target.value })}
       />
-      <Button sx={styles.button} style={{ width: '95%' }}>
+      <Button onClick={sendForm} sx={styles.button} style={{ width: '95%' }}>
         Send
       </Button>
     </div>
