88b1befa1d489de14c83f59d9e695f110eab942c
Brian Liu <bwliu@outlook.com>
2023-07-01 21:43:31 -0700

    Implemented GET method for /events

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -3,7 +3,21 @@ import asyncHandler from 'express-async-handler';
 
 // Display list of all Events.
 export const eventList = asyncHandler(async (req, res) => {
-  res.send('NOT IMPLEMENTED: Event list');
+  try {
+    // Retrieve events from database
+    const events = await Event.find();
+
+    // Extract attributes from each event 
+    const eventAttributes = events.map((event) => {
+      const { title, status, start_time, end_time, location, description, calendar_link, instagram_link  } = event;
+      return { title, status, start_time, end_time, location, description, calendar_link, instagram_link };
+    });
+
+    // Return array of all events and their attributes
+    res.json(eventAttributes);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error'});
+  }
 });
 
 // Display detail for a specific Event.
