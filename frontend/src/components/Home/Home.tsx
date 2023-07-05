6a26c7045ee0df2a233baa98c8ce28104231e41e
SithuSoe04 <63730799+SithuSoe04@users.noreply.github.com>
2023-07-05 22:05:09 +0630

    Fixed a few things

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -14,10 +14,10 @@ const Home = () => {
       <Grid container>
         <Grid item xs={12} sm={5} md={5}>
           <Box sx={{ textAlign: {xs: 'center', sm: 'left'}}}>
-            <Box sx={{ ...styles.title, marginTop: {xs: '8%', sm: '20%', md: '25%'}, marginLeft: {xs: '0%', sm: '0%', md: '12%'}}}>Innovate.</Box>
-            <Box sx={{...styles.title, marginLeft: {xs: '0%', sm: '0%', md: '12%'}}}>Build.</Box>
-            <Box sx={{...styles.title, marginLeft: {xs: '0%', sm: '0%', md: '12%'}}}>Connect.</Box>
-            <Box sx={{...styles.button, marginLeft: {xs: '0%', sm: '0%', md: '12%'}}}><Button size="large" text="Learn About Us" onClick={() => console.log('click')}/></Box>
+            <Box sx={{ ...styles.title, marginTop: {xs: '8%', sm: '20%', md: '25%'}, marginLeft: {xs: '0%', sm: '3%', md: '12%'}}}>Innovate.</Box>
+            <Box sx={{...styles.title, marginLeft: {xs: '0%', sm: '3%', md: '12%'}}}>Build.</Box>
+            <Box sx={{...styles.title, marginLeft: {xs: '0%', sm: '3%', md: '12%'}}}>Connect.</Box>
+            <Box sx={{...styles.button, marginLeft: {xs: '0%', sm: '2%', md: '12%'}}}><Button size="large" text="Learn About Us" onClick={() => console.log('click')}/></Box>
           </Box>
         </Grid>
           <Grid item xs={12} sm={7} md={6.5}>
@@ -25,36 +25,36 @@ const Home = () => {
           </Grid>
       </Grid>
       <Grid container spacing={0} sx={{marginTop: '17%'}}>
-        <Grid item xs={12} sm={5}>
-          <Box sx={{...styles.subtitle, marginTop: '1%', display: 'flex', justifyContent: {xs: 'center', sm: 'right'}}}>Join CSES today.</Box>
+        <Grid item xs={12} sm={4.5} md={5}>
+          <Box sx={{...styles.subtitle, display: 'flex', justifyContent: {xs: 'center', sm: 'right'}}}>Join CSES today.</Box>
           <Box sx={{...styles.button, marginTop: {xs: '3%', sm: '0%'}, display: 'flex', justifyContent: {xs: 'center', sm: 'right'}, marginLeft: '0%'}}><Button size="large" text="Become a Member ->" onClick={() => console.log('click')}/></Box>
         </Grid>
-        <Grid item xs={12} sm={2} sx={{ display: {xs: 'flex', sm: "block"}, justifyContent: 'center', alignItems: 'center'}}>
+        <Grid item xs={11} sm={2.5} md={2} sx={{ display: {xs: 'flex', sm: "block"}, justifyContent: 'center', alignItems: 'center'}}>
           <Box sx={{...styles.statisticContainer, width: {xs: '100%', sm: 'auto'}}}>
             <Box sx={styles.statisticWrapper}>
-              <Box sx={styles.statisticTitle}>1000+</Box>
-              <Box sx={styles.statisticSubtitle}>Members & counting.</Box>
+              <Box sx={{...styles.statisticTitle, fontSize: {xs: 'clamp(40px, 3vw, 100px)', sm: 'clamp(20px, 3vw, 30px)', md: 'clamp(15px, 3vw, 50px)'}}}>1000+</Box>
+              <Box sx={{...styles.statisticSubtitle, fontSize: {xs: 'clamp(15px, 3vw, 50px)', sm: 'clamp(10px, 3vw, 12px)', md: 'clamp(12px, 3vw, 17px)'}}}>Members & counting.</Box>
             </Box>
           </Box>
         </Grid>
-        <Grid item xs={12} sm={2}>
+        <Grid item xs={11} sm={2.5} md={2}>
           <Box sx={styles.statisticContainer}>
             <Box sx={styles.statisticWrapper}>
-              <Box sx={styles.statisticTitle}>n +</Box>
-              <Box sx={styles.statisticSubtitle}>Another statistic!</Box>
+              <Box sx={{...styles.statisticTitle, fontSize: {xs: 'clamp(40px, 3vw, 100px)', sm: 'clamp(20px, 3vw, 30px)', md: 'clamp(12px, 3vw, 50px)'}}}>n+</Box>
+              <Box sx={{...styles.statisticSubtitle, fontSize: {xs: 'clamp(15px, 3vw, 50px)', sm: 'clamp(10px, 3vw, 12px)', md: 'clamp(12px, 3vw, 17px)'}}}>Another statistic!</Box>
             </Box>
           </Box>
         </Grid>
-          <Grid item xs={12} sm={2}>
+          <Grid item xs={11} sm={2.5} md={2}>
             <Box sx={styles.statisticContainer}>
               <Box sx={styles.statisticWrapper}>
-                <Box sx={styles.statisticTitle}>1000+</Box>
-                <Box sx={styles.statisticSubtitle}>Members & counting.</Box>
+                <Box sx={{...styles.statisticTitle, fontSize: {xs: 'clamp(40px, 3vw, 100px)', sm: 'clamp(20px, 3vw, 30px)', md: 'clamp(12px, 3vw, 50px)'}}}>1000+</Box>
+                <Box sx={{...styles.statisticSubtitle, fontSize: {xs: 'clamp(15px, 3vw, 50px)', sm: 'clamp(10px, 3vw, 12px)', md: 'clamp(12px, 3vw, 17px)'}}}>Members & counting.</Box>
               </Box>
             </Box>
           </Grid>
         </Grid>       
-        <Box sx={{ color: 'white', fontSize: 'clamp(20px, 5vw, 60px)', fontFamily: 'Chakra Petch', marginLeft: '5%', marginTop: '17%', fontWeight: 700,}}>UPCOMING EVENTS.</Box>
+        <Box sx={{ color: 'white', fontSize: 'clamp(25px, 5vw, 60px)', fontFamily: 'Chakra Petch', marginLeft: {sx: '5%', sm: '2%', md: '5%'}, marginTop: '17%', fontWeight: 700, textAlign: {xs: 'center', sm: 'left', md: 'left'}}}>UPCOMING EVENTS.</Box>
       </Container>
     </Box>
   );
