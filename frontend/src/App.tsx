2b10abbe1e8e74c64d134441970097dc3dc2f09e
Will-Hsu <46422881+Will-Hsu@users.noreply.github.com>
2023-07-08 08:50:53 -0700

    Milestone Two File Set Up (#18)
    * added text elements for footer
    
    * more styles added to forms
    
    * front end more less done
    
    * fix nav bar sizes
    
    * clean up footer styles
    
    * Footer UI done
    
    * add alerts for invalid input
    
    * footer feedback form linked to cses email
    
    * added animation and close for alerts and fix hover color
    
    * add linktree
    
    * add discord
    
    * create starter files for new pages
    
    * added backgroun
    
    * milestone two starter files created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,11 @@ import { Container } from '@mui/material';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import About from './components/About/About';
+import Events from './components/Events/Events';
+import Opportunities from './components/Opportunities/Opportunities';
+import Membership from './components/Membership/Membership';
+import Login from './components/Login/Login';
 
 function App() {
   return (
@@ -12,6 +17,11 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/opportunities" element={<Opportunities />} />
+          <Route path="/membership" element={<Membership />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </Container>
