ea0e1a63ab7e01364174d761ea3adc7f6f558c85
joycellu <jol072@ucsd.edu>
2023-07-12 22:39:20 +0800

    update

--- frontend/src/components/About/styles.ts
+++ frontend/src/components/About/styles.ts
@@ -7,5 +7,142 @@ export const aboutStyles = () => ({
     marginRight: '-1%',
     scale: '103%',
   },
-  body: { position: 'absolute', top: '93px' },
+  body: { position: 'absolute', top: '93px'},
+
+  root: {
+    position: 'relative',
+    overflow: 'auto',
+  },
+
+  button1: {
+    color: 'white',
+    fontSize: '21px',
+    fontWeight: 500,
+    fontFamily: 'Chakra Petch',
+    marginLeft: { xs: '0%', sm: '3%', md: '19%' },
+    marginTop: { xs: '8%', sm: '20%', md: '3%' },
+  },
+
+  button2: {
+    color: 'white',
+    fontSize: '21px',
+    fontWeight: 500,
+    fontFamily: 'Chakra Petch',
+    marginLeft: { xs: '0%', sm: '3%', md: '19%' },
+    marginTop: { xs: '8%', sm: '20%', md: '-6%' },
+  },
+
+  titleTop: {
+    color: 'white',
+    fontSize: 'clamp(20px, 8vw, 65px)',
+    fontFamily: 'Chakra Petch',
+    marginLeft: { xs: '0%', sm: '3%', md: '20%' },
+    marginTop: { xs: '8%', sm: '20%', md: '38%' },
+    fontWeight: 700,
+  },
+
+  subtitleTop: {
+    color: 'white',
+    fontSize: 'clamp(10px, 5vw, 20px)',
+    fontFamily: 'Inter',
+    marginLeft: { xs: '0%', sm: '3%', md: '20%' },
+    marginTop: { xs: '8%', sm: '20%', md: '5%' },
+    fontWeight: 400,
+    width: '430px',
+  },
+
+  mainTitleMiddle: {
+    color: 'white',
+    fontSize: 'clamp(20px, 8vw, 65px)',
+    fontFamily: 'Chakra Petch',
+    marginLeft: { xs: '0%', sm: '3%', md: '35%' },
+    marginTop: { xs: '8%', sm: '20%', md: '20%' },
+    fontWeight: 700,
+  },
+
+  titleBottom1: {
+    color: 'white',
+    fontSize: 'clamp(20px, 8vw, 48px)',
+    fontFamily: 'Inter',
+    marginLeft: { xs: '0%', sm: '3%', md: '20%' },
+    marginTop: { xs: '8%', sm: '20%', md: '20%' },
+    fontWeight: 600,
+  },
+
+  subtitleBottom1: {
+    color: 'white',
+    fontSize: 'clamp(10px, 5vw, 20px)',
+    fontFamily: 'Inter',
+    marginLeft: { xs: '0%', sm: '3%', md: '20%' },
+    marginTop: { xs: '8%', sm: '20%', md: '3%' },
+    fontWeight: 400,
+    width: '573px',
+  },
+
+  titleBottom2: {
+    color: 'white',
+    fontSize: 'clamp(20px, 8vw, 48px)',
+    fontFamily: 'Inter',
+    marginLeft: { xs: '0%', sm: '3%', md: '50%' },
+    marginTop: { xs: '8%', sm: '20%', md: '30%' },
+    fontWeight: 600,
+  },
+
+  subtitleBottom2: {
+    color: 'white',
+    fontSize: 'clamp(10px, 5vw, 20px)',
+    fontFamily: 'Inter',
+    marginLeft: { xs: '0%', sm: '3%', md: '50%' },
+    marginTop: { xs: '8%', sm: '20%', md: '3%' },
+    fontWeight: 400,
+    width: '573px',
+  },
+
+  titleBottom3: {
+    color: 'white',
+    fontSize: 'clamp(20px, 8vw, 48px)',
+    fontFamily: 'Inter',
+    marginLeft: { xs: '0%', sm: '3%', md: '20%' },
+    marginTop: { xs: '8%', sm: '20%', md: '20%' },
+    fontWeight: 600,
+  },
+
+  subtitleBottom3: {
+    color: 'white',
+    fontSize: 'clamp(10px, 5vw, 20px)',
+    fontFamily: 'Inter',
+    marginLeft: { xs: '0%', sm: '3%', md: '20%' },
+    marginTop: { xs: '8%', sm: '20%', md: '3%' },
+    fontWeight: 400,
+    width: '573px',
+  },
+
+  titleBottom4: {
+    color: 'white',
+    fontSize: 'clamp(20px, 8vw, 48px)',
+    fontFamily: 'Inter',
+    marginLeft: { xs: '0%', sm: '3%', md: '39%' },
+    marginTop: { xs: '8%', sm: '20%', md: '23%' },
+    fontWeight: 600,
+  },
+
+  subtitleBottom4: {
+    color: 'white',
+    fontSize: 'clamp(10px, 5vw, 20px)',
+    fontFamily: 'Inter',
+    marginLeft: { xs: '0%', sm: '3%', md: '39%' },
+    marginTop: { xs: '8%', sm: '20%', md: '3%' },
+    fontWeight: 400,
+    width: '573px',
+  },
+
+  mainTitleBottom: {
+    color: 'white',
+    fontSize: 'clamp(20px, 8vw, 65px)',
+    fontFamily: 'Chakra Petch',
+    marginLeft: { xs: '0%', sm: '3%', md: '35%' },
+    marginTop: { xs: '8%', sm: '20%', md: '10%' },
+    fontWeight: 700,
+  },
+
 });
