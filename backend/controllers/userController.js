9cf638d6bc2344bdbdf0af8cbc03cdd825955f48
Eddie Ho <noodleslove@icloud.com>
2023-07-28 18:38:48 -0700

    Implemented userUpdate & userDelete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import User from '../models/user.js';
-import { body } from 'express-validator';
+import { body, param, validationResult } from 'express-validator';
 import asyncHandler from 'express-async-handler';
 
 export const userCheck = [
@@ -50,26 +50,79 @@ export const getUserInfo = asyncHandler(async (req, res) => {
   }
 });
 
-// TODO: implement this
-export const userRead = asyncHandler(async (req, res) => {
-  res.send('TODO: read user');
-});
+// Handle User update on PUT.
+export const userUpdate = [
+  param('email')
+    .notEmpty()
+    .withMessage('Email must be specificed.')
+    .isEmail()
+    .withMessage('Invalid email.'),
+  body('name').optional(),
+  body('major').optional(),
+  body('expectedGraduationYear').optional().isNumeric(),
 
-// TODO: implement this
-export const userUpdate = asyncHandler(async (req, res) => {
-  res.send('TODO: update user');
-});
+  asyncHandler(async (req, res) => {
+    console.log('its working');
+    const error = validationResult(req);
 
-// TODO: implement this
-export const userDelete = asyncHandler(async (req, res) => {
-  res.send('TODO: delete user');
-});
+    if (!error.isEmpty()) {
+      // There are errors.
+      return res.status(400).json({ message: 'Failed', errors: error.array() });
+    }
+    // Data from param and body is valid.
+    const { email } = req.params;
+
+    // Query database for user with given ID
+    const user = await User.findOne({ email }).exec();
+
+    if (!user) {
+      // User not found.
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      // Update the record.
+      User.updateOne({ email: req.params.email }, req.body)
+        .then(() => res.json({ message: 'Successful' }))
+        .catch(() => res.status(500).json({ message: 'Internal server error' }));
+    }
+  }),
+];
+
+// Handle User delete on DELETE.
+export const userDelete = [
+  // Validate and sanitize fields.
+  param('email', 'Email must not be empty.').notEmpty().isEmail().withMessage('Invalid email.'),
+
+  asyncHandler(async (req, res) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      res.status(400).json({ message: 'Failed', errors: errors.array() });
+    }
+    // Data from param is valid.
+    const { email } = req.params;
+
+    // Query database for user with given email
+    const user = await User.findOne({ email }).exec();
+
+    if (!user) {
+      // User not found.
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      // User found, delete it.
+      user
+        .deleteOne()
+        .then(() => res.json({ message: 'Successful' }))
+        .catch(() => res.status(500).json({ message: 'Internal server error' }));
+    }
+  }),
+];
 
 export default {
   userCheck,
   userCreate,
   getUserInfo,
-  userRead,
   userUpdate,
   userDelete,
 };
