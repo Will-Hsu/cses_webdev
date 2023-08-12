704022a161476a97085c87b3e5c75df26a43d463
kaung-min-khant <1kaungminkhant1@gmail.com>
2023-08-11 20:33:35 -0700

    Events Page Responsiveness

--- frontend/src/components/Events/styles.ts
+++ frontend/src/components/Events/styles.ts
@@ -1,27 +1,30 @@
 export const event_style = () => ({
-  bg1: { width: '100%', margin: '10% 0%', transform: 'rotate(-25deg)', scale: '135%' },
-  bg2: { width: '100%', transform: 'scaleX(-1) scaleY(-1)', margin: '30% 0%' },
-  body: { position: 'absolute', top: '93px' },
-  // Add the new styles for the event container
-  eventContainer: {
+  bg1: {
+    opacity: '0.8',
+    width: '100%',
+    margin: '10% 0%',
+    transform: 'rotate(-25deg)',
+    scale: '135%',
+  },
+  bg2: {
+    opacity: '0.8',
+    width: '100%',
+    transform: 'scaleX(-1) scaleY(-1)',
+    margin: '30% 0%',
+  },
+  eventsContainer: {
     color: 'white',
     fontSize: '20px',
     fontFamily: 'Chakra Petch',
     fontWeight: '700',
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'row' as 'row',
+    flexWrap: 'wrap' as 'wrap' | 'nowrap' | 'wrap-reverse',
     marginLeft: '39px',
-    flexWrap: 'wrap',
-    maxWidth: '1000px',
+    maxWidth: '1024px',
+    overflowX: 'scroll' as 'scroll' | 'auto' | 'hidden' | 'visible' | 'inherit' | 'initial' | 'revert' | 'unset',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr)',
+    gap: '40px',
   },
-
-  // New style for the event box container
-  eventBoxContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    gap: '20px',
-    maxWidth: '1000px',
-    margin: '0 auto',
-  },
+  body: { position: 'absolute', top: '93px' },
 });
