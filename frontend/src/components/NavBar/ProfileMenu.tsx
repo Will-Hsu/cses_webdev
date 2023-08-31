9dc9d7cd211cbcef83a8f24435798f215b12a091
SithuSoe04 <63730799+SithuSoe04@users.noreply.github.com>
2023-08-31 23:15:28 +0630

    Feature/sithu soe edit/fe edit profile (#67)
    * connect to backend member profile
    
    * fix event style
    
    * Made props more concise
    
    * connect navbar profilepic to backend
    
    * update home style
    
    * fix events background
    
    * events fix
    
    * fix redirect lag
    
    * fix nav bar
    
    * Updated events dashboard to only appear for admin
    
    * Edit functionality implemented
    
    * Fixed leaderboard layout & connect backend
    
    * Fixed section title for mobile mode
    
    * add new sponsor
    
    * fixed margin
    
    * Fix
    
    * Added EditProfile page
    
    * fix the end point
    
    ---------
    
    Co-authored-by: joycellu <jol072@ucsd.edu>
    Co-authored-by: Will-Hsu <46422881+Will-Hsu@users.noreply.github.com>
    Co-authored-by: Brian Liu <bwliu@outlook.com>
    Co-authored-by: Eddie Ho <noodleslove@icloud.com>

--- frontend/src/components/NavBar/ProfileMenu.tsx
+++ frontend/src/components/NavBar/ProfileMenu.tsx
@@ -1,16 +1,22 @@
 import { Menu, MenuItem } from '@mui/material';
+import { AuthContext } from '../../context/AuthContext';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface ProfileMenuProps {
     anchorEl: null | HTMLElement;
     onClose: () => void;
 };
 
+
 const ProfileMenu: React.FC<ProfileMenuProps> = ({ anchorEl, onClose }) => {
+    const navigate = useNavigate();
+    const { logout } = useContext(AuthContext);
     return (
         <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={onClose}>
-            <MenuItem onClick={onClose}>Edit Profile</MenuItem>
+            <MenuItem onClick={()=>{navigate("/editprofile",{ replace: true })}}>Edit Profile</MenuItem>
             <MenuItem onClick={onClose}>Settings</MenuItem>
-            <MenuItem onClick={onClose}>Logout</MenuItem>
+            <MenuItem onClick={(logout)}>Logout</MenuItem>
         </Menu>
     );
 };
