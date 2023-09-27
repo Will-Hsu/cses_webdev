8c0f60b459f07431067235796d3631ea5bcd9433
Eddie Ho <62191480+noodleslove@users.noreply.github.com>
2023-09-27 11:57:22 +0800

    Implement RedeemLog & log with redeem API (#84)
    * Implement RedeemLog & log with redeem API
    
    * Added user email to redeemLog schema
    
    * pull from latest
    
    ---------
    
    Co-authored-by: Will-Hsu <46422881+Will-Hsu@users.noreply.github.com>

---
+++ backend/models/redeemLog.js
@@ -0,0 +1,12 @@
+import { Schema, model } from 'mongoose';
+
+const redeemLogSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  userEmail: { type: String, required: true },
+  redeemType: { type: String, required: true, enum: ['small', 'medium', 'large'] },
+  timestamp: { type: Date, required: true },
+  pointsBefore: Number,
+  pointsAfter: Number,
+});
+
+export default model('RedeemLog', redeemLogSchema);
