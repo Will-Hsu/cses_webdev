0b064d7b0e7249b3b36cf39a0bac348673f533c5
Brian Liu <bwliu@outlook.com>
2024-02-11 19:07:27 -0800

    Initial API connection set up

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './database/connect-db.js';
 // import testDatabaseConnection from './database/db-test.js';
+import connectMailchimp from './mailchimp/connect-mailchimp.js';
 
 // import routes
 import eventRoutes from './routes/event.js';
@@ -25,6 +26,9 @@ const baseApi = '/api/v1';
 
 // Connect to MongoDB
 connectDB();
+
+// Connect to Mailchimp
+connectMailchimp();
 
 app.get('/', function (_, res) {
   res.send('CSES Web Dev Backend');
