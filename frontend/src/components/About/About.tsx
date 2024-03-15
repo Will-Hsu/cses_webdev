1b3b26f497b4a2480280e1096711d9d85f44d481
Victor Kim <17victork@gmail.com>
2024-03-14 20:40:07 -0700

    redo how image and text are rendered using conditional direction alignment

--- frontend/src/components/About/About.tsx
+++ frontend/src/components/About/About.tsx
@@ -204,10 +204,45 @@ const About = () => {
               </Box>
             </Grid>
           </Grid> */}
+          <Grid 
+          container 
+          justifyContent='center' 
+          mt={12} mb={12} 
+          direction={isSmallScreen ? 'column-reverse' : 'row'} >
+            <Grid item sm={5}>
+              <Box sx={{ color: 'white', textAlign: { md: 'left', sm: 'left', xs: 'center' } }}>
+                <h1>How do I join?</h1>
+              </Box>
+              <Box sx={{ color: 'white', textAlign: { lg: 'left', sm: 'left', xs: 'center' } }}>
+                <p style={{ color: 'white', fontSize: 'clamp(15px, 3vw, 20px)' }}>
+                  To become a general member, simply sign up with your UCSD email!
+                </p>
+                <p style={{ color: 'white', fontSize: 'clamp(15px, 3vw, 20px)' }}>
+                  Do you want to be a part of the internal team? Become a member and follow us on
+                  our socials to be notified of when board applications open on a rolling basis.
+                </p>
+                <Box
+                  sx={{
+                    marginLeft: '-2%',
+                    display: 'flex',
+                    justifyContent: { xs: 'center', sm: 'left' },
+                  }}
+                >
+                  <Button
+                    size="large"
+                    text="Become a Member ->"
+                    onClick={() => navigate('/membership')}
+                  />
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item sm={5}>
               <ImageWithBoxShadow 
                 src={about3}
                 alt="img"
               />
+            </Grid>
+          </Grid>
           <Communities />
           <MeetTheTeam />
         </Container>
