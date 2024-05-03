8c9f8111e3688be989faf41e39d44158acb128de
Will-Hsu <46422881+Will-Hsu@users.noreply.github.com>
2024-05-03 10:30:28 -0700

    remove old members

--- frontend/src/components/About/MeetTheTeam.tsx
+++ frontend/src/components/About/MeetTheTeam.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Box, Grid, ToggleButton, createTheme, useMediaQuery } from '@mui/material';
-import shape from '../../images/shape.svg';
 import previous from '../../images/previous.png';
 import next from '../../images/next.png';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
@@ -14,27 +13,19 @@ import Rahul from '../../images/meettheteamImages/rahul.png';
 import Ishika from '../../images/meettheteamImages/ishika.jpg';
 import Yashil from '../../images/meettheteamImages/yashil vora.jpg';
 import Cassandra from '../../images/meettheteamImages/cassandra.png';
-import Sidhant from '../../images/meettheteamImages/sidhant.jpg';
 import Rachel from '../../images/meettheteamImages/rachel paner.jpeg';
 import Christopher from '../../images/meettheteamImages/christopher.png';
-import Gaurav from '../../images/meettheteamImages/gaurav.jpg';
 import Ketan from '../../images/meettheteamImages/ketan.jpg';
-import Anay from '../../images/meettheteamImages/anay.jpeg';
-import Vinuthna from '../../images/meettheteamImages/vinuthna.jpg';
 import Gloria from '../../images/meettheteamImages/gloria.jpg';
 import Mishka from '../../images/meettheteamImages/mishka.jpeg';
 import Delvin from '../../images/meettheteamImages/delvin.jpg';
-import Sadrac from '../../images/meettheteamImages/sadrac.jpg';
 import Joyce from '../../images/meettheteamImages/joyce.jpg';
-import Eddie from '../../images/meettheteamImages/eddie.jpeg';
-import Kaung from '../../images/meettheteamImages/kaung.png';
 import Will from '../../images/meettheteamImages/will.jpg';
 import Brian from '../../images/meettheteamImages/brian.jpg';
 import Shruti from '../../images/meettheteamImages/shruti.jpg';
 import Manan from '../../images/meettheteamImages/manan.jpg';
 import Jheel from '../../images/meettheteamImages/jheel.jpg';
 import Sonia from '../../images/meettheteamImages/sonia.jpg';
-import Nishant from '../../images/meettheteamImages/nishant.jpg';
 import Jake from '../../images/meettheteamImages/jake villaseno.jpeg';
 import Jose from '../../images/meettheteamImages/josue martinez.jpeg';
 import Angelina from '../../images/meettheteamImages/angelina yee.jpg';
@@ -44,7 +35,6 @@ import Sardor from '../../images/meettheteamImages/sardor sobirov.jpg';
 import Shreya from '../../images/meettheteamImages/shreya gupta.jpg';
 import ShreyaN from '../../images/meettheteamImages/Shreya Nagunuri.png';
 import Ryan from '../../images/meettheteamImages/ryan rickey.jpg';
-//.import Saleha from '../../images/meettheteamImages/DSC_1386 - Saleha Ahmedi.jpg';
 import Viren from '../../images/meettheteamImages/viren nathan.jpg';
 import Kevin from '../../images/meettheteamImages/kevin kim.jpeg';
 import KevinW from '../../images/meettheteamImages/KevinWu.png';
@@ -54,7 +44,6 @@ import Ali from '../../images/meettheteamImages/ali alani.png';
 import Michael from '../../images/meettheteamImages/michael he.png';
 import Cole from '../../images/meettheteamImages/cole.png';
 import Victor from '../../images/meettheteamImages/VictorKim.jpeg';
-import Sancho from '../../images/meettheteamImages/SanchoS.png';
 
 const categories = [
   {
@@ -76,7 +65,7 @@ const categories = [
       { name: 'Rachel Paner', title: 'Design Chair', photo: Rachel },
       { name: 'Jose Martinez', title: 'Outreach Chair - Alumni', photo: Jose },
       { name: 'Ketan Jain', title: 'Outreach Chair - Student Organizations', photo: Ketan },
-      { name: 'Cole Carter', title: 'Outreach Chair - Sponsors/Corporates', photo: Cole }
+      { name: 'Cole Carter', title: 'Outreach Chair - Sponsors/Corporates', photo: Cole },
     ],
   },
   {
@@ -137,7 +126,7 @@ const MeetTheTeam = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const isMiddleScreen = useMediaQuery(theme.breakpoints.between('sm', 'md'));
   const isSmallScreen = useMediaQuery(theme.breakpoints.between('xs', 'sm'));
-  const verySmallScreen = useMediaQuery('(max-width:350px')
+  const verySmallScreen = useMediaQuery('(max-width:350px');
 
   const membersPerPage = 6;
   const remainingSpots = membersPerPage - (currentCategory.members.length % membersPerPage);
@@ -187,7 +176,10 @@ const MeetTheTeam = () => {
           MEET THE TEAM!
         </Box>
       </Grid>
-      <Grid container sx={{ marginTop: '5%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Grid
+        container
+        sx={{ marginTop: '5%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
         <ToggleButtonGroup value={currentCategory.name} exclusive aria-label="Category Selection">
           {categories.map((category) => (
             <ToggleButton
@@ -303,8 +295,8 @@ const MeetTheTeam = () => {
                 >
                   <div
                     style={{
-                      width: verySmallScreen ? '120px': '140px',
-                      height: verySmallScreen ? '120px': '140px',
+                      width: verySmallScreen ? '120px' : '140px',
+                      height: verySmallScreen ? '120px' : '140px',
                       overflow: 'hidden',
                       display: 'flex',
                       justifyContent: 'center',
